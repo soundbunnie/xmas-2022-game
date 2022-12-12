c7318ab612be3578385ebf7cba4309119b7bf836
Aria Elkaim <ariari.elk@gmail.com>
2022-12-12 03:25:05 -0500

    Add ASCII art that says cassidys home

--- game-disks/cassidy-disk.js
+++ game-disks/cassidy-disk.js
@@ -23,11 +23,21 @@ const cassidyDisk = () => ({
       name: 'YOUR HOUSE',     // This will be displayed each time the player enters the room.
       id: 'playerHouse',        // The unique identifier for this room. Entering a room will set the disk's roomId to this.
       img: `
-      ___    _    ___  ___  ___  ___ __   __ _  ___   _  _   ___   __  __  ___ 
-      / __|  /_\  / __|/ __||_ _||   \\ \ / /( )/ __| | || | / _ \ |  \/  || __|
-     | (__  / _ \ \__ \\__ \ | | | |) |\ V / |/ \__ \ | __ || (_) || |\/| || _| 
-      \___|/_/ \_\|___/|___/|___||___/  |_|     |___/ |_||_| \___/ |_|  |_||___|
-                                                                                
+      #####     #     #####   #####  ### ######  #     # ###  #####  
+      #     #   # #   #     # #     #  #  #     #  #   #  ### #     # 
+      #        #   #  #       #        #  #     #   # #    #  #       
+      #       #     #  #####   #####   #  #     #    #    #    #####  
+      #       #######       #       #  #  #     #    #              # 
+      #     # #     # #     # #     #  #  #     #    #        #     # 
+       #####  #     #  #####   #####  ### ######     #         #####  
+                                                                 
+                    #     # ####### #     # ####### 
+                    #     # #     # ##   ## #       
+                    #     # #     # # # # # #       
+                    ####### #     # #  #  # #####   
+                    #     # #     # #     # #       
+                    #     # #     # #     # #       
+                    #     # ####### #     # #######                                
       `,
       desc: `
         Congratulations, IkariDude09! You have defeated the evil emperor Zylzyx and restored peace to the k†ngdøm.
